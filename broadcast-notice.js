bb0260411c742aeea181324d9163b1feaaca301a
omarmy <ankitburdak05@gmail.com>
2026-09-09 19:42:31 +0530

    Broadcast JS: card2

--- broadcast-notice.js
+++ broadcast-notice.js
@@ -1,10 +1,10 @@
 window.__REMOTE_BROADCAST_NOTICE__ = {
-  "id": "notice-2026-09-09T14-11-25-card2",
+  "id": "notice-2026-09-09T14-12-27-card2",
   "active": true,
   "card": "card2",
   "icon": "💎",
   "title": "Card 2 • 3D Model",
   "message": "kesa laga new card",
   "btnText": "Mast Hai 🔥",
-  "timestamp": "2026-09-09T14:11:25.394Z"
+  "timestamp": "2026-09-09T14:12:27.349Z"
 };
